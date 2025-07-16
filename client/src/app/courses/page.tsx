207dcb3d62887382a20c32b586db9bb4f0d4155f
kadar-naruto <khadarpaashe123@gmail.com>
2025-07-16 15:34:25 +0300

    Features added:  * - NavigationFixed component with fixed positioning and shadow styling.  * - Search bar with Input field and Search icon.  * - Logo, Browse button, and authentication buttons.

---
+++ client/src/app/courses/page.tsx
@@ -0,0 +1,10 @@
+import { NavigationFixed } from '@/components/components/navigation'
+import React from 'react'
+
+ const Page = () => {
+  return (
+   <NavigationFixed/>
+  )
+}
+
+export default Page

5c22728b49f9836721492d6fc7109af8397bcb86
kadar-naruto <khadarpaashe123@gmail.com>
2025-08-25 10:38:55 +0300

    fixed linting issues

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import upload from "../utils/multer.ts";
+import type { Router } from "express";
 
 import { forgetPassword, resetPassword } from "../controllers/authController.ts";
 import express from "express";
 import { getAllUsers, updateProfile, changepassword, updateProfileImage, changeUserRole } from "../controllers/usersController.ts";
 import { protectRoute } from "../middlewares/protectRoute.ts"
 
-const userRouter = express.Router()
+const userRouter: Router = express.Router()
 
 userRouter.patch("/forgetpassword", forgetPassword)
 userRouter.route("/resetpassword/:token").patch(resetPassword)

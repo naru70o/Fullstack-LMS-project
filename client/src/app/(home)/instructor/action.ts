0dd8210c8d48de8389d44f58620d1652aa6e07f8
naruto <khadarpaashe123@gmail.com>
2025-10-16 09:29:46 +0300

    made the selected state scalable

--- client/src/app/(home)/instructor/action.ts
+++ client/src/app/(home)/instructor/action.ts
@@ -1,6 +1,6 @@
 "use server";
 
 export async function addinstructor(formdata: FormData) {
-  console.log(formdata.get("expertice"));
+  console.log(formdata.get("occupation"));
   console.log(formdata.get("expertise"));
 }
--- client/src/app/(home)/instructor/step-one/page.tsx
+++ client/src/app/(home)/instructor/step-one/page.tsx
@@ -5,49 +5,51 @@ import Form from "../_components/form";
 import { InstructorFormInput } from "../_components/instructor-form-input";
 import StepButton from "../_components/step-button";
 import { MultiSelect } from "../_components/multi-select";
-
-export type Option = {
-  label: string;
-  value: string;
-  category?: string;
-};
+import { expertiseOptions, fruits, Option } from "../_libs/options";
 
 export default function page() {
-  const [selected, setSelected] = useState<Option[]>([]);
+  const [selected, setSelected] = useState<{
+    occupation: Option[];
+    expertise: Option[];
+  }>({
+    occupation: [],
+    expertise: [],
+  });
 
-  const options: Option[] = [
-    { label: "Apple", value: "apple", category: "Fruits" },
-    { label: "Banana", value: "banana", category: "Fruits" },
-    { label: "Cherry", value: "cherry", category: "Fruits" },
-    { label: "Date", value: "date", category: "Fruits" },
-    { label: "Elderberry", value: "elderberry", category: "Fruits" },
-    { label: "Carrot", value: "carrot", category: "Vegetables" },
-    { label: "Broccoli", value: "broccoli", category: "Vegetables" },
-    { label: "Spinach", value: "spinach", category: "Vegetables" },
-    { label: "Potato", value: "potato", category: "Vegetables" },
-    { label: "Tomato", value: "tomato", category: "Vegetables" },
-    { label: "Milk", value: "milk", category: "Dairy" },
-    { label: "Cheese", value: "cheese", category: "Dairy" },
-    { label: "Yogurt", value: "yogurt", category: "Dairy" },
-  ];
+  const handleSelectionChange = (
+    field: keyof typeof selected,
+    value: Option[]
+  ) => {
+    setSelected((prev) => ({ ...prev, [field]: value }));
+  };
+
+  console.log(selected);
   return (
     <section className="container h-screen grid grid-cols-1 content-center justify-center px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto py-[60px]">
       <div className="mx-auto flex flex-col items-start lg:flex lg:flex-row gap-4">
         <StepNavigation />
         <Form>
-          <InstructorFormInput
+          {/* <InstructorFormInput
             type="text"
             name="expertice"
             placeholder="expertice"
             description="Select all that apply (e.g., Programming, Design, Music, etc.)"
+          /> */}
+          <MultiSelect
+            discription="your occupation"
+            options={expertiseOptions}
+            selected={selected.occupation}
+            name="occupation"
+            onChange={(value) => handleSelectionChange("occupation", value)}
           />
           <MultiSelect
             discription="your expertice"
-            options={options}
-            selected={selected}
+            options={fruits}
+            selected={selected.expertise}
             name="expertise"
-            onChange={setSelected}
+            onChange={(value) => handleSelectionChange("expertise", value)}
           />
+
           <StepButton step="Next step" />
         </Form>
       </div>

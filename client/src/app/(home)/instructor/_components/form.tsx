9c0e230267ad1b1c01c3b5dbe92d3f157e884d43
naruto <khadarpaashe123@gmail.com>
2025-10-20 15:51:22 +0300

    created zod schema and options fuction formatter

--- client/src/app/(home)/instructor/_components/form.tsx
+++ client/src/app/(home)/instructor/_components/form.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import { addinstructor } from "../action";
+import React, { useActionState } from "react";
+import { registerInstructorOne } from "../action";
 
 export default function Form({ children }: { children: React.ReactNode }) {
+  const [_state, formAction, pending] = useActionState(
+    registerInstructorOne,
+    null
+  );
+
   return (
     <div className="w-full flex flex-col items-center gap-4 mt-8 lg:mt-0">
       <form
-        action={addinstructor}
+        action={formAction}
         className="flex flex-col gap-4 w-[300px] md:w-xl"
       >
         {children}
@@ -13,36 +18,3 @@ export default function Form({ children }: { children: React.ReactNode }) {
     </div>
   );
 }
-
-/*
-      <form className="flex flex-col gap-4 w-[300px] md:w-xl">
-        <input
-          type="text"
-          placeholder="full name"
-          className="bg-[var(--input-bg-color)] w-full max-w-xl p-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary-color)] text-[var(--input-text-color)] font-poppins text-[16px] font-normal leading-[24px]"
-        />
-        <input
-          type="text"
-          placeholder="display name"
-          className="bg-[var(--input-bg-color)] w-full max-w-xl p-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary-color)] text-[var(--input-text-color)] font-poppins text-[16px] font-normal leading-[24px]"
-        />
-        <input
-          type="email"
-          placeholder="john.doe@example.com"
-          className="bg-[var(--input-bg-color)] w-full max-w-xl p-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-[var(--primary-color)] text-[var(--input-text-color)] font-poppins text-[16px] font-normal leading-[24px]"
-        />
-        <SelectInput
-          className="w-full max-w-full"
-          selectItems={["somali", "english"]}
-          selectLabel="language"
-          selectPlaceholder="Select a language"
-        />
-        <Button
-          size="lg"
-          variant="primary"
-          className="w-fit self-end bg-[var(--primary-color)] text-white hover:bg-primary/80 focus:ring-2 focus:ring-[var(--primary-color)] cursor-pointer mt-2"
-        >
-          Next step
-        </Button>
-      </form>
-      */
--- client/src/app/(home)/instructor/action.ts
+++ client/src/app/(home)/instructor/action.ts
@@ -2,23 +2,44 @@
 
 import z from "zod";
 import { formatZodErrors, stepOneSchema } from "./zodTypes";
+import { redirect } from "next/navigation";
 
-export async function addinstructor(formdata: FormData) {
+interface ObjType {
+  label: string;
+  value: string;
+  category: string;
+}
+
+function formatSelectOptions(obj: any) {
+  const deserialesed = JSON.parse(obj);
+  const values = deserialesed.map((option: ObjType) => option.value);
+  console.log(values);
+  return values;
+}
+
+export async function registerInstructorOne(
+  prev,
+  formdata: FormData
+): Promise<void | string | Record<string, string>> {
   try {
+    formatSelectOptions(formdata.getAll("occupation"));
     const data = {
-      occupation: formdata.getAll("occupation"),
-      specificSkills: formdata.getAll("specificSkills"),
-      yearsOfExpertise: formdata.get("yearsOfExpertise"),
-      qualification: formdata.getAll("qualification"),
+      occupation: formatSelectOptions(formdata.getAll("occupation")),
+      specificSkills: formatSelectOptions(formdata.getAll("specificSkills")),
+      yearsOfExpertise: (formdata.get("yearsOfExpertise") as string) || null,
+      qualification: formatSelectOptions(formdata.getAll("qualification")),
     };
+
+    console.log(data);
     const validated = stepOneSchema.parse(data);
-    console.log(validated);
-  } catch (error) {
+    redirect("instructor/step-two");
+  } catch (error: unknown) {
     if (error instanceof z.ZodError) {
       const formatedZoderrors = formatZodErrors(error);
       console.log(formatedZoderrors);
       return formatedZoderrors;
     } else {
+      console.error(error);
       return "Something went wrong";
     }
   }
